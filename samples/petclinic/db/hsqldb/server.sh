f4c058b602b7be8ea69ac5830a176aa8040624b6
kimchy <kimchy@gmail.com>
2006-02-22 19:28:37 +0000

    some idea formatting

--- samples/petclinic/db/hsqldb/server.sh
+++ samples/petclinic/db/hsqldb/server.sh
@@ -1,1 +1,1 @@
-java -classpath ../../../../modules/gps/lib/hsqldb-1.7.3.0.jar org.hsqldb.Server -database petclinic
+java -classpath ../../../../lib/hsqldb/hsqldb.jar org.hsqldb.Server -database petclinic
