5baafeaec4b4fea8ed28691eab930764eb83cd27
kimchy <kimchy@gmail.com>
2006-02-23 01:26:13 +0000

    jpa reference documentation + fetchCount support for JpaDevice

--- src/tiger/src/org/compass/gps/device/jpa/JpaGpsDevice.java
+++ src/tiger/src/org/compass/gps/device/jpa/JpaGpsDevice.java
@@ -16,6 +16,10 @@
 
 package org.compass.gps.device.jpa;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import org.compass.core.CompassSession;
 import org.compass.core.util.Assert;
 import org.compass.gps.CompassGpsException;
@@ -27,51 +31,43 @@ import org.compass.gps.device.jpa.entities.JpaEntitiesLocatorDetector;
 import org.compass.gps.device.jpa.lifecycle.JpaEntityLifecycleInjector;
 import org.compass.gps.device.jpa.lifecycle.JpaEntityLifecycleInjectorDetector;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
-import java.util.List;
-
 /**
  * A Java Persistence API Gps Device (EJB3 Persistence).
- * <p>
- * The jpa device provides support for using jpa to index a database.
- * The path can be viewed as: Database <-> EntityManager(JPA) <-> Objects <-> Compass::Gps
- * <-> Compass::Core (Search Engine).
- * What it means is that for every object that has both jpa and compass
- * mappings, you will be able to index it's data, as well as real time mirroring
- * of data changes.
- * <p>
+ * <p/>
+ * The jpa device provides support for using jpa to index a database. The path can be viewed as: Database <->
+ * EntityManager(JPA) <-> Objects <-> Compass::Gps <-> Compass::Core (Search Engine). What it means is that for every
+ * object that has both jpa and compass mappings, you will be able to index it's data, as well as real time mirroring of
+ * data changes.
+ * <p/>
  * When creating the object, an <code>EntityManagerFactory</code> must be provided to the Device.
- * <p>
- * Indexing uses {@link JpaEntitiesLocator} to locate all the entities that can be indexed
- * (i.e. entities that have both Compass and JPA mappings). Most of the time the
- * {@link org.compass.gps.device.jpa.entities.DefaultJpaEntitiesLocator} is enough,
- * but special JPA implementation one can be provided. If none is provided, the device will
- * use the {@link JpaEntitiesLocatorDetector} to auto detect the correct locator (which defaults
- * to the ({@link org.compass.gps.device.jpa.entities.DefaultJpaEntitiesLocator}).
- * <p>
- * Mirroring can be done in two ways. The first one is using JPA official API, implemeting an
- * Entity Lifecycle listener and specifing it for each entity class via annotations. Compass
- * comes with helper base clases for it, {@link AbstractCompassJpaEntityListener} and
- * {@link AbstractDeviceJpaEntityListener}. As far as integrating Compass with JPA for mirroring,
- * this is the less preferable way. The second option for mirroring is to use the {@link JpaEntityLifecycleInjector},
- * which will use the internal JPA implementation to inject global lifecycle event listerens (sadly, there
- * is no option to do that with the <code>EntityManagerFactory</code> API). If the {@link #setInjectEntityLifecycleListener(boolean)}
- * is set to <code>true</code> (defaults to <code>false</code>), the device will try to use the injector
- * to inject global event listeners. If no {@link JpaEntityLifecycleInjector} is defined, the device will try
- * to autodetect the injector based on the current support for specific JPA implementations using the
- * {@link JpaEntityLifecycleInjectorDetector}. See its javadoc for a list of the current JPA implementations supported.
- * <p>
- * Mirroring can be turned off using the {@link #setMirrorDataChanges(boolean)} to <code>false</code>. It
- * defaults to <code>true<code>.
- * <p>
- * The device allows for {@link NativeEntityManagerFactoryExtractor} to be set, for applications that use a
- * framework or by themself wrap the actual <code>EntityManagerFactory</code> implementation.
- * <p>
- * For advance usage, the device allows for {@link EntityManagerWrapper} to be set, allowing to control the
- * creation of <code>EntityManager</code>s, and transactions. The {@link DefaultEntityManagerWrapper} should
- * suffice for most cases.
+ * <p/>
+ * Indexing uses {@link JpaEntitiesLocator} to locate all the entities that can be indexed (i.e. entities that have both
+ * Compass and JPA mappings). Most of the time the {@link org.compass.gps.device.jpa.entities.DefaultJpaEntitiesLocator}
+ * is enough, but special JPA implementation one can be provided. If none is provided, the device will use the {@link
+ * JpaEntitiesLocatorDetector} to auto detect the correct locator (which defaults to the ({@link
+ * org.compass.gps.device.jpa.entities.DefaultJpaEntitiesLocator}).
+ * <p/>
+ * Mirroring can be done in two ways. The first one is using JPA official API, implemeting an Entity Lifecycle listener
+ * and specifing it for each entity class via annotations. Compass comes with helper base clases for it, {@link
+ * AbstractCompassJpaEntityListener} and {@link AbstractDeviceJpaEntityListener}. As far as integrating Compass with JPA
+ * for mirroring, this is the less preferable way. The second option for mirroring is to use the {@link
+ * JpaEntityLifecycleInjector}, which will use the internal JPA implementation to inject global lifecycle event
+ * listerens (sadly, there is no option to do that with the <code>EntityManagerFactory</code> API). If the {@link
+ * #setInjectEntityLifecycleListener(boolean)} is set to <code>true</code> (defaults to <code>false</code>), the device
+ * will try to use the injector to inject global event listeners. If no {@link JpaEntityLifecycleInjector} is defined,
+ * the device will try to autodetect the injector based on the current support for specific JPA implementations using
+ * the {@link JpaEntityLifecycleInjectorDetector}. See its javadoc for a list of the current JPA implementations
+ * supported.
+ * <p/>
+ * Mirroring can be turned off using the {@link #setMirrorDataChanges(boolean)} to <code>false</code>. It defaults to
+ * <code>true<code>.
+ * <p/>
+ * The device allows for {@link NativeEntityManagerFactoryExtractor} to be set, for applications that use a framework or
+ * by themself wrap the actual <code>EntityManagerFactory</code> implementation.
+ * <p/>
+ * For advance usage, the device allows for {@link EntityManagerWrapper} to be set, allowing to control the creation of
+ * <code>EntityManager</code>s, and transactions. The {@link DefaultEntityManagerWrapper} should suffice for most
+ * cases.
  *
  * @author kimchy
  */
@@ -87,6 +83,8 @@ public class JpaGpsDevice extends AbstractGpsDevice implements PassiveMirrorGpsD
     }
 
     private boolean mirrorDataChanges = true;
+
+    private int fetchCount = 200;
 
     private EntityManagerFactory entityManagerFactory;
 
@@ -134,14 +132,24 @@ public class JpaGpsDevice extends AbstractGpsDevice implements PassiveMirrorGpsD
         }
         for (EntityInformation entityInformation : entetiesInformation) {
             try {
+                int current = 0;
                 entityManagerWrapper.open();
                 EntityManager entityManager = entityManagerWrapper.getEntityManager();
-                Query query = entityManager.createQuery(entityInformation.getSelectQuery());
-                List results = query.getResultList();
-                for (Object result : results) {
-                    session.create(result);
+                while (true) {
+                    Query query = entityManager.createQuery(entityInformation.getSelectQuery());
+                    query.setFirstResult(current);
+                    query.setMaxResults(current + fetchCount);
+                    List results = query.getResultList();
+                    for (Object result : results) {
+                        session.create(result);
+                    }
+                    session.evictAll();
+                    entityManager.clear();
+                    if (results.size() < fetchCount) {
+                        break;
+                    }
+                    current += fetchCount;
                 }
-                session.evictAll();
                 entityManagerWrapper.close();
             } catch (Exception e) {
                 log.error(buildMessage("Failed to index the database"), e);
@@ -162,10 +170,21 @@ public class JpaGpsDevice extends AbstractGpsDevice implements PassiveMirrorGpsD
         this.mirrorDataChanges = mirrorDataChanges;
     }
 
+    /**
+     * Sets the Jpa <code>EntityManagerFactory</code>. This is manadatory for the Jpa device.
+     *
+     * @param entityManagerFactory The entity manager factory the device will use.
+     */
     public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
         this.entityManagerFactory = entityManagerFactory;
     }
 
+    /**
+     * Sets the Entity Manager factory wrapper to control the entity manager operations. This is mandatory since the
+     * device has sensible defaults for it.
+     *
+     * @param entityManagerWrapper The entity manager wrapper to control the manager operations.
+     */
     public void setEntityManagerWrapper(EntityManagerWrapper entityManagerWrapper) {
         this.entityManagerWrapper = entityManagerWrapper;
     }
@@ -188,5 +207,15 @@ public class JpaGpsDevice extends AbstractGpsDevice implements PassiveMirrorGpsD
 
     public void setEntitiesLocator(JpaEntitiesLocator entitiesLocator) {
         this.entitiesLocator = entitiesLocator;
+    }
+
+    /**
+     * Sets the fetch count for the indexing process. A large number will perform the indexing faster, but will consume
+     * more memory. Defaults to <code>200</code>.
+     *
+     * @param fetchCount
+     */
+    public void setFetchCount(int fetchCount) {
+        this.fetchCount = fetchCount;
     }
 }
