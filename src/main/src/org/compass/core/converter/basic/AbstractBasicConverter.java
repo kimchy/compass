535a909943dbd52e465278e115ce36c65cb68166
kimchy <kimchy@gmail.com>
2006-02-25 10:47:01 +0000

    CMP-167 : Add possibility to index and search null value fields.

--- src/main/src/org/compass/core/converter/basic/AbstractBasicConverter.java
+++ src/main/src/org/compass/core/converter/basic/AbstractBasicConverter.java
@@ -39,10 +39,10 @@ public abstract class AbstractBasicConverter implements ResourcePropertyConverte
         SearchEngine searchEngine = context.getSearchEngine();
 
         // don't save a null value if the context does not states so
-        if (root == null && !context.handleNulls()) {
+        if (root == null && !handleNulls(context)) {
             return;
         }
-        String sValue = searchEngine.getNullValue();
+        String sValue = getNullValue(context);
         if (root != null) {
             sValue = toString(root, resourcePropertyMapping);
         }
@@ -64,13 +64,54 @@ public abstract class AbstractBasicConverter implements ResourcePropertyConverte
         Property p = resource.getProperty(propertyName);
 
         // don't set anything if null
-        if (p == null || searchEngine.isNullValue(p.getStringValue())) {
+        if (p == null || isNullValue(context, p.getStringValue())) {
             return null;
         }
 
         return fromString(p.getStringValue(), resourcePropertyMapping);
     }
 
+    /**
+     * Should the converter handle nulls? Handling nulls means should the
+     * converter process nulls or not. Usually the converter will not
+     * persist null values, but sometimes it might be needed
+     * ({@link org.compass.core.marshall.MarshallingContext#handleNulls()}).
+     * <p/>
+     * Extracted to a method so special converters can control null handling.
+     *
+     * @param context
+     * @return <code>true</code> if the converter should handle null values
+     */
+    protected boolean handleNulls(MarshallingContext context) {
+        return context.handleNulls();
+    }
+
+    /**
+     * If the converter handle nulls, the value that will be stored in the
+     * search engine for <code>null</code> values (during the marshall process).
+     *
+     * @param context
+     * @return Null value that will be inserted for <code>null</code>s.
+     */
+    protected String getNullValue(MarshallingContext context) {
+        return context.getSearchEngine().getNullValue();
+    }
+
+    /**
+     * Is the value read from the search engine is a <code>null</code> value
+     * during the unmarshall process.
+     *
+     * @param context
+     * @param value The value to check for <code>null</code> value.
+     * @return <code>true</code> if the value represents a null value.
+     */
+    protected boolean isNullValue(MarshallingContext context, String value) {
+        return context.getSearchEngine().isNullValue(value);
+    }
+
+    /**
+     * Default implementation of toString, simply calls the Object toString.
+     */
     public String toString(Object o, ResourcePropertyMapping resourcePropertyMapping) {
         return o.toString();
     }
