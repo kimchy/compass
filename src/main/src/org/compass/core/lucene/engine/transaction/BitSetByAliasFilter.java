49469905b5c0680c98bb63790978fc41be903c6c
kimchy <kimchy@gmail.com>
2006-10-29 15:42:38 +0000

    better performance when performing deletes (still needs more work)

--- src/main/src/org/compass/core/lucene/engine/transaction/BitSetByAliasFilter.java
+++ src/main/src/org/compass/core/lucene/engine/transaction/BitSetByAliasFilter.java
@@ -29,12 +29,122 @@ import org.apache.lucene.search.Filter;
 /**
  * A Lucene filter which stored deletion (per alias), and filters them when
  * execution lucene operations.
- * 
+ *
  * @author kimchy
  */
+// TODO This should be removed and we should use HitCollector
+// TODO We are using ArrayList with Integer to record deletion, we should have a growable int[] for better performance
 public class BitSetByAliasFilter extends Filter {
 
+    public static class AllSetBitSet extends BitSet {
+
+        public AllSetBitSet() {
+        }
+
+        public int cardinality() {
+            throw new UnsupportedOperationException();
+        }
+
+        public int hashCode() {
+            return System.identityHashCode(this);
+        }
+
+        public int length() {
+            throw new UnsupportedOperationException();
+        }
+
+        public int size() {
+            return super.size();    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        public void clear() {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean isEmpty() {
+            return false;
+        }
+
+        public int nextClearBit(int fromIndex) {
+            throw new UnsupportedOperationException();
+        }
+
+        public int nextSetBit(int fromIndex) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void clear(int bitIndex) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void set(int bitIndex) {
+            // do nothing
+        }
+
+        public boolean get(int bitIndex) {
+            return true;
+        }
+
+        public void clear(int fromIndex, int toIndex) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void flip(int fromIndex, int toIndex) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void set(int fromIndex, int toIndex) {
+            // do nothing
+        }
+
+        public void set(int fromIndex, int toIndex, boolean value) {
+            // do nothing
+        }
+
+        public void set(int bitIndex, boolean value) {
+            // do nothing
+        }
+
+        public Object clone() {
+            return super.clone();    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        public boolean equals(Object obj) {
+            throw new UnsupportedOperationException();
+        }
+
+        public String toString() {
+            return "AllBitSet";
+        }
+
+        public BitSet get(int fromIndex, int toIndex) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void and(BitSet set) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void andNot(BitSet set) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void or(BitSet set) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void xor(BitSet set) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean intersects(BitSet set) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
     private static final long serialVersionUID = 3618980083415921974L;
+
+    private static final AllSetBitSet allSetBitSet = new AllSetBitSet();
 
     private HashMap bitSets = new HashMap();
 
@@ -82,20 +192,13 @@ public class BitSetByAliasFilter extends Filter {
 
     public BitSet bits(IndexReader reader) throws IOException {
         if (!(reader instanceof HasSubIndexReader)) {
-            return allBits(reader);
+            return allSetBitSet;
         }
         HasSubIndexReader hasSubIndexReader = (HasSubIndexReader) reader;
         BitSet bitSet = (BitSet) bitSets.get(hasSubIndexReader.getSubIndex());
         if (bitSet != null) {
             return bitSet;
         }
-        return allBits(reader);
-    }
-
-    private BitSet allBits(IndexReader reader) {
-        int maxDoc = reader.maxDoc();
-        BitSet bitSet = new BitSet(maxDoc);
-        bitSet.set(0, maxDoc, true);
-        return bitSet;
+        return allSetBitSet;
     }
 }
