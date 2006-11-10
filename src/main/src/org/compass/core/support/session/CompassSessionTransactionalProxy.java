6e6638efccb5e7935b1fbe208bc4b5574ed4ab04
kimchy <kimchy@gmail.com>
2006-11-10 07:19:24 +0000

    better javadocs

--- src/main/src/org/compass/core/support/session/CompassSessionTransactionalProxy.java
+++ src/main/src/org/compass/core/support/session/CompassSessionTransactionalProxy.java
@@ -39,10 +39,25 @@ public class CompassSessionTransactionalProxy implements InvocationHandler, Seri
 
     private Compass compass;
 
+    /**
+     * Constrcuts a new invocation handler based on the session. Allows for
+     * proxing {@link org.compass.core.CompassSession} for simpler API usage
+     * <b>within a transactional context</b>.
+     *
+     * @param compass The Compass instance to use for session creation
+     */
     public CompassSessionTransactionalProxy(Compass compass) {
         this.compass = compass;
     }
 
+    /**
+     * Creates a new proxied {@link org.compass.core.CompassSession} that can
+     * be used within an already running transcational context without worrying
+     * about session management API or transcation management API.
+     *
+     * @param compass The compass instance used to create the session
+     * @return A proxied session for simpler session usage within an already running transactional context
+     */
     public static CompassSession newProxy(Compass compass) {
         return (CompassSession) Proxy.newProxyInstance(
                 InternalCompassSession.class.getClassLoader(),
@@ -50,6 +65,12 @@ public class CompassSessionTransactionalProxy implements InvocationHandler, Seri
                 new CompassSessionTransactionalProxy(compass));
     }
 
+    /**
+     * Since Compass already does most of the session management, this wrapper around
+     * {@link org.compass.core.CompassSession} invocation simply uses {@link org.compass.core.Compass#openSession()}
+     * and invoke the appropiate method on the session returned. Compass will make sure that this is the correct
+     * session within the context.
+     */
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if (method.getName().equals("equals")) {
             // Only consider equal when proxies are identical.
