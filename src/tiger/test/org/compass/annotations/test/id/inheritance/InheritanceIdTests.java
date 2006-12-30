2de1de31695dc221a4d7a7141736c65e77271d05
kimchy <kimchy@gmail.com>
2006-12-30 12:18:12 +0000

    another tests for annotations and ids

---
+++ src/tiger/test/org/compass/annotations/test/id/inheritance/InheritanceIdTests.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2004-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.annotations.test.id.inheritance;
+
+import org.compass.annotations.test.AbstractAnnotationsTestCase;
+import org.compass.core.CompassSession;
+import org.compass.core.CompassTransaction;
+import org.compass.core.config.CompassConfiguration;
+
+/**
+ * @author kimchy
+ */
+public class InheritanceIdTests extends AbstractAnnotationsTestCase {
+
+    protected void addExtraConf(CompassConfiguration conf) {
+        conf.addClass(B.class);
+    }
+
+    public void testInheritanceId() throws Exception {
+        CompassSession session = openSession();
+        CompassTransaction tr = session.beginTransaction();
+
+        B b = new B();
+        b.id = 1;
+        b.value = "test";
+        session.save(b);
+
+        b = (B) session.load(B.class, 1);
+        assertEquals(1, b.id);
+        assertEquals("test", b.value);
+
+        tr.commit();
+        session.close();
+    }
+}
