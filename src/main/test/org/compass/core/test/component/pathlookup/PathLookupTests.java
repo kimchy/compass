4e5fa0a54a13810aacc82b8abefb80d27267297e
kimchy <kimchy@gmail.com>
2007-01-22 12:04:49 +0000

    more tests for path lookup with component mappings

---
+++ src/main/test/org/compass/core/test/component/pathlookup/PathLookupTests.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.test.component.pathlookup;
+
+import org.compass.core.CompassHits;
+import org.compass.core.CompassSession;
+import org.compass.core.CompassTransaction;
+import org.compass.core.test.AbstractTestCase;
+
+/**
+ * @author kimchy
+ */
+public class PathLookupTests extends AbstractTestCase {
+
+    protected String[] getMappings() {
+        return new String[]{"component/pathlookup/mapping.cpm.xml"};
+    }
+
+    public void testBLookup() {
+        CompassSession session = openSession();
+        CompassTransaction tr = session.beginTransaction();
+
+        A a = new A(1, new B("test"));
+        session.save(a);
+
+        CompassHits hits = session.queryBuilder().term("a.b.value", "test").hits();
+        assertEquals(1, hits.length());
+
+        tr.commit();
+        session.close();
+    }
+}
