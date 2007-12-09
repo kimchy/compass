a204dfc409710231774e38b53b290d451323bec9
kimchy <kimchy@gmail.com>
2007-12-09 23:34:29 +0000

    CMP-490
    ScrollableHibernateIndexEntitiesIndexer fails to close hibernateSession causing DataSource connection leak when performing index operations

--- src/main/src/org/compass/gps/device/hibernate/indexer/ScrollableHibernateIndexEntitiesIndexer.java
+++ src/main/src/org/compass/gps/device/hibernate/indexer/ScrollableHibernateIndexEntitiesIndexer.java
@@ -152,6 +152,7 @@ public class ScrollableHibernateIndexEntitiesIndexer implements HibernateIndexEn
                 }
                 throw (HibernateGpsDeviceException) e;
             } finally {
+                hibernateSession.close();
                 session.close();
             }
         }
