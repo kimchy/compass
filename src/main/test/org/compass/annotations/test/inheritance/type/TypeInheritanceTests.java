5bac8ba67d2f15deaf06934ebbaa99b5a195dec0
kimchy <kimchy@gmail.com>
2008-02-29 12:56:38 +0000

    added type inheritance test for annotations

---
+++ src/main/test/org/compass/annotations/test/inheritance/type/TypeInheritanceTests.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2004-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.annotations.test.inheritance.type;
+
+import org.compass.annotations.test.AbstractAnnotationsTestCase;
+import org.compass.core.CompassSession;
+import org.compass.core.CompassTransaction;
+import org.compass.core.config.CompassConfiguration;
+
+/**
+ * @author kimchy
+ */
+public class TypeInheritanceTests extends AbstractAnnotationsTestCase {
+
+    protected void addExtraConf(CompassConfiguration conf) {
+        conf.addClass(B.class);
+    }
+
+    public void testTypeInheritance() {
+        CompassSession session = openSession();
+        CompassTransaction tr = session.beginTransaction();
+
+        B b = new B();
+        b.setId("test");
+        session.save(b);
+
+        b = session.load(B.class, "test");
+        assertNotNull(b);
+
+        tr.commit();
+        session.close();
+    }
+}
