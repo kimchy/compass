0251d46c407e6f54b224adf8ae5e47dd251494b0
kimchy <kimchy@gmail.com>
2008-06-19 07:57:35 +0000

    fuzzy lookup test

---
+++ src/main/test/org/compass/core/test/querybuilder/fuzzylookup/FuzzyLookupTests.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.test.querybuilder.fuzzylookup;
+
+import org.compass.core.CompassSession;
+import org.compass.core.CompassTransaction;
+import org.compass.core.test.AbstractTestCase;
+
+/**
+ * @author kimchy
+ */
+public class FuzzyLookupTests extends AbstractTestCase {
+
+    protected String[] getMappings() {
+        return new String[]{"querybuilder/fuzzylookup/mapping.cpm.xml"};
+    }
+
+    public void testFuzzyLookup() {
+        CompassSession session = openSession();
+        CompassTransaction tr = session.beginTransaction();
+
+        A a = new A();
+        a.id = 1;
+        a.value = "text";
+        session.save(a);
+
+        assertEquals(1, session.find("test~0.5").length());
+
+        tr.commit();
+        session.close();
+    }
+}
