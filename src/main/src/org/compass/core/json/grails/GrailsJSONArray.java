f2749b6fef11acae034dbce0f857e1f01d09f8bf
kimchy <kimchy@gmail.com>
2008-06-19 22:12:06 +0000

    CMP-654 Json to Search Engine Mapping Support
    added supprot for grails json objects

---
+++ src/main/src/org/compass/core/json/grails/GrailsJSONArray.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.json.grails;
+
+import org.codehaus.groovy.grails.web.json.JSONArray;
+import org.compass.core.json.JsonArray;
+
+/**
+ * A wrapper around grails {@link org.codehaus.groovy.grails.web.json.JSONArray}. 
+ *
+ * @author kimchy
+ */
+public class GrailsJSONArray implements JsonArray {
+
+    private JSONArray jsonArray;
+
+    public GrailsJSONArray(JSONArray jsonArray) {
+        this.jsonArray = jsonArray;
+    }
+
+    public int length() {
+        return jsonArray.length();
+    }
+
+    public boolean isNull(int index) {
+        return jsonArray.isNull(index);
+    }
+
+    public Object opt(int index) {
+        return jsonArray.opt(index);
+    }
+}
---
+++ src/main/src/org/compass/core/json/grails/converter/GrailsContentConverter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2004-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.json.grails.converter;
+
+import org.codehaus.groovy.grails.web.json.JSONException;
+import org.compass.core.converter.ConversionException;
+import org.compass.core.converter.json.JsonContentConverter;
+import org.compass.core.json.AliasedJsonObject;
+import org.compass.core.json.JsonObject;
+import org.compass.core.json.grails.GrailsAliasedJSONObject;
+
+/**
+ * Converts a {@link org.compass.core.json.JsonObject} to String and a String to
+ * {@link org.compass.core.json.grails.GrailsAliasedJSONObject}.
+ *
+ * @author kimchy
+ */
+public class GrailsContentConverter implements JsonContentConverter {
+
+    public String toJSON(JsonObject jsonObject) throws ConversionException {
+        return jsonObject.toString();
+    }
+
+    public AliasedJsonObject fromJSON(String alias, String json) throws ConversionException {
+        try {
+            return new GrailsAliasedJSONObject(alias, json);
+        } catch (JSONException e) {
+            throw new ConversionException("Failed to convert json: " + json + " with alias [" + alias + "]", e);
+        }
+    }
+}
