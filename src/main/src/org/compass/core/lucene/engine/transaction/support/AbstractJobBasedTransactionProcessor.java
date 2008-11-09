5b2d5eb9be63adf62f25341c6483ca76aa482447
kimchy <kimchy@gmail.com>
2008-11-09 17:14:30 +0000

    added transaciton job based base classes

---
+++ src/main/src/org/compass/core/lucene/engine/transaction/support/AbstractJobBasedTransactionProcessor.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2004-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.lucene.engine.transaction.support;
+
+import org.compass.core.engine.SearchEngineException;
+import org.compass.core.lucene.engine.LuceneSearchEngine;
+import org.compass.core.spi.InternalResource;
+import org.compass.core.spi.ResourceKey;
+
+/**
+ * Base class for jobs based ({@link org.compass.core.lucene.engine.transaction.support.TransactionJobs})
+ * transaction processor. Mainly used for simple accumelating of {@link org.compass.core.lucene.engine.transaction.support.TransactionJob}s
+ * and then processing them during commit/rollback time.
+ *
+ * @author kimchy
+ */
+public abstract class AbstractJobBasedTransactionProcessor extends AbstractSearchTransactionProcessor {
+
+    private TransactionJobs jobs;
+
+    public AbstractJobBasedTransactionProcessor(LuceneSearchEngine searchEngine) {
+        super(searchEngine);
+    }
+
+    public void begin() throws SearchEngineException {
+        // nothing to do here
+    }
+
+    public void prepare() throws SearchEngineException {
+        if (jobs == null) {
+            return;
+        }
+        doPrepare(jobs);
+    }
+
+    abstract protected void doPrepare(TransactionJobs jobs) throws SearchEngineException;
+
+    public void commit(boolean onePhase) throws SearchEngineException {
+        if (jobs == null) {
+            return;
+        }
+        doCommit(onePhase, jobs);
+    }
+
+    abstract protected void doCommit(boolean onePhase, TransactionJobs jobs) throws SearchEngineException;
+
+    public void rollback() throws SearchEngineException {
+        if (jobs == null) {
+            return;
+        }
+        doRollback(jobs);
+    }
+
+    abstract protected void doRollback(TransactionJobs jobs) throws SearchEngineException;
+
+    public void flush() throws SearchEngineException {
+    }
+
+    public void create(InternalResource resource) throws SearchEngineException {
+        getTransactionJobs().add(new TransactionJob(TransactionJob.Type.CREATE, resource));
+    }
+
+    public void update(InternalResource resource) throws SearchEngineException {
+        getTransactionJobs().add(new TransactionJob(TransactionJob.Type.UPDATE, resource));
+    }
+
+    public void delete(ResourceKey resourceKey) throws SearchEngineException {
+        getTransactionJobs().add(new TransactionJob(TransactionJob.Type.DELETE, resourceKey));
+    }
+
+    private TransactionJobs getTransactionJobs() {
+        if (jobs != null) {
+            jobs = new TransactionJobs();
+        }
+        return jobs;
+    }
+
+}
