da290026e5e013fea536cdae5ea4dfa37e8a42cb
kimchy <kimchy@gmail.com>
2008-12-20 12:24:24 +0000

    CMP-797 Programmatic API for building JSEM definition

--- src/main/src/org/compass/core/mapping/json/builder/JsonAllMappingBuilder.java
+++ src/main/src/org/compass/core/mapping/json/builder/JsonAllMappingBuilder.java
@@ -21,46 +21,84 @@ import org.compass.core.mapping.SpellCheckType;
 import org.compass.core.mapping.internal.DefaultAllMapping;
 
 /**
+ * A builder allowing to constrcut resource all mapping definition.
+ * 
  * @author kimchy
  */
 public class JsonAllMappingBuilder {
 
     final DefaultAllMapping mapping;
 
+    /**
+     * Constructs a new all mapping builder.
+     */
     public JsonAllMappingBuilder() {
         this.mapping = new DefaultAllMapping();
     }
 
+    /**
+     * Should the all mapping be enabled or not. Defaults to the global
+     * {@link org.compass.core.config.CompassEnvironment.All#ENABLED} setting which
+     * in turn defaults to <code>true</code>.
+     */
     public JsonAllMappingBuilder enable(boolean enable) {
         mapping.setSupported(enable);
         return this;
     }
 
+    /**
+     * Should the alias be exlcuded from the all mapping or not. Default to the
+     * {@link org.compass.core.config.CompassEnvironment.All#EXCLUDE_ALIAS} setting which in turn
+     * defaults to <code>true</code>.
+     */
     public JsonAllMappingBuilder excludeAlias(boolean excludeAlias) {
         mapping.setExcludeAlias(excludeAlias);
         return this;
     }
 
+    /**
+     * Should the all mapping include properties that do not have mappings. Defaults to the global
+     * {@link org.compass.core.config.CompassEnvironment.All#INCLUDE_UNMAPPED_PROPERTIES} setting which
+     * in turn defaults to <code>true</code>.
+     */
     public JsonAllMappingBuilder includePropertiesWithNoMappings(boolean includePropertiesWithNoMappings) {
         mapping.setIncludePropertiesWithNoMappings(includePropertiesWithNoMappings);
         return this;
     }
 
+    /**
+     * The term vector of the all property. Default to the
+     * {@link org.compass.core.config.CompassEnvironment.All#TERM_VECTOR} setting which in turn
+     * defaults to <code>NO</code>.
+     */
     public JsonAllMappingBuilder termVector(Property.TermVector termVector) {
         mapping.setTermVector(termVector);
         return this;
     }
 
+    /**
+     * The omit norms of the all property. Default to the
+     * {@link org.compass.core.config.CompassEnvironment.All#OMIT_NORMS} setting which in turn
+     * defaults to <code>false</code>.
+     */
     public JsonAllMappingBuilder omitNorms(boolean omitNorms) {
         mapping.setOmitNorms(omitNorms);
         return this;
     }
 
+    /**
+     * The omit tf of the all property. Default to the
+     * {@link org.compass.core.config.CompassEnvironment.All#OMIT_TF} setting which in turn
+     * defaults to <code>false</code>.
+     */
     public JsonAllMappingBuilder omitTf(boolean omitTf) {
         mapping.setOmitTf(omitTf);
         return this;
     }
 
+    /**
+     * The spell check type of the all property.
+     */
     public JsonAllMappingBuilder spellCheck(SpellCheckType spellCheck) {
         mapping.setSpellCheck(spellCheck);
         return this;
--- src/main/src/org/compass/core/mapping/json/builder/JsonIdMappingBuilder.java
+++ src/main/src/org/compass/core/mapping/json/builder/JsonIdMappingBuilder.java
@@ -19,19 +19,23 @@ package org.compass.core.mapping.json.builder;
 import org.compass.core.converter.Converter;
 import org.compass.core.converter.mapping.ResourcePropertyConverter;
 import org.compass.core.converter.mapping.support.FormatDelegateConverter;
-import org.compass.core.engine.naming.StaticPropertyPath;
 import org.compass.core.mapping.ExcludeFromAllType;
 import org.compass.core.mapping.SpellCheckType;
 import org.compass.core.mapping.json.JsonIdMapping;
-import org.compass.core.mapping.json.NamingType;
 
 /**
+ * A builder allowing to constrcut json id mapping definition.
+ *
  * @author kimchy
+ * @see JSEM#id(String)
  */
 public class JsonIdMappingBuilder {
 
     final JsonIdMapping mapping;
 
+    /**
+     * Constructs a new json id mapping builder for the specified name.
+     */
     public JsonIdMappingBuilder(String name) {
         this.mapping = new JsonIdMapping();
         mapping.setName(name);
@@ -39,67 +43,98 @@ public class JsonIdMappingBuilder {
         mapping.setOmitTf(true);
     }
 
-    public JsonIdMappingBuilder indexName(String indexName) {
-        mapping.setPath(new StaticPropertyPath(indexName));
-        return this;
-    }
-
-    public JsonIdMappingBuilder namingType(NamingType namingType) {
-        mapping.setNamingType(namingType);
-        return this;
-    }
-
+    /**
+     * If set, omit normalization factors associated with this indexed field.
+     * This effectively disables indexing boosts and length normalization for this field. By
+     * default, it is set for id mapping.
+     */
     public JsonIdMappingBuilder omitNorms(boolean omitNorms) {
         mapping.setOmitNorms(omitNorms);
         return this;
     }
 
+    /**
+     * If set, omit tf from postings of this indexed property. By default, it is set for
+     * id mapping.
+     */
     public JsonIdMappingBuilder omitTf(boolean omitTf) {
         mapping.setOmitTf(omitTf);
         return this;
     }
 
+    /**
+     * Sets the boost value for the id property mapping. Defaults to <code>1.0f</code>.
+     */
     public JsonIdMappingBuilder boost(float boost) {
         mapping.setBoost(boost);
         return this;
     }
 
+    /**
+     * Sets the format that will be used for formattable capable converters (such as numbers and dates).
+     */
     public JsonIdMappingBuilder format(String format) {
         mapping.setValueConverter(new FormatDelegateConverter(format));
         mapping.setFormat(format);
         return this;
     }
 
+    /**
+     * Sets the lookup converter name (registered with Compass) that will be used to convert the JSON
+     * property. Defaults to {@link org.compass.core.converter.mapping.json.JsonIdMappingConverter}.
+     */
     public JsonIdMappingBuilder converter(String converterName) {
         mapping.setConverterName(converterName);
         return this;
     }
 
+    /**
+     * Sets the actual converter that will be used to convert the JSON
+     * property. Defaults to {@link org.compass.core.converter.mapping.json.JsonIdMappingConverter}.
+     */
     public JsonIdMappingBuilder converter(Converter converter) {
         mapping.setConverter(converter);
         return this;
     }
 
+    /**
+     * Sets the lookup converter name (registered with Compass) that will be used to convert the actual
+     * value of the json property. Detaults to {@link org.compass.core.converter.json.SimpleJsonValueConverter}.
+     */
     public JsonIdMappingBuilder valueConverter(String converterName) {
         mapping.setValueConverterName(converterName);
         return this;
     }
 
+    /**
+     * Sets the actual converter that will be used to convert the actual
+     * value of the json property. Detaults to {@link org.compass.core.converter.json.SimpleJsonValueConverter}.
+     */
     public JsonIdMappingBuilder valueConverter(Converter converter) {
         mapping.setValueConverter(converter);
         return this;
     }
 
+    /**
+     * Sets the actual converter that will be used to convert the actual
+     * value of the json property. Detaults to {@link org.compass.core.converter.json.SimpleJsonValueConverter}.
+     */
     public JsonIdMappingBuilder valueConverter(ResourcePropertyConverter converter) {
         mapping.setValueConverter(converter);
         return this;
     }
 
+    /**
+     * Controls if the id property will be excluded from all or not.
+     */
     public JsonIdMappingBuilder excludeFromAll(ExcludeFromAllType excludeFromAll) {
         mapping.setExcludeFromAll(excludeFromAll);
         return this;
     }
 
+    /**
+     * Sets the spell check specific setting for the mapping.
+     */
     public JsonIdMappingBuilder spellCheck(SpellCheckType spellCheck) {
         mapping.setSpellCheck(spellCheck);
         return this;
