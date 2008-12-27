a9b8d84e4a6275ca80ede74a71461cd7b9e67c76
kimchy <kimchy@gmail.com>
2008-12-27 00:17:39 +0000

    CMP-802 Programmatic API for building OSEM definition

--- src/main/src/org/compass/core/mapping/osem/builder/SearchableIdComponentMappingBuilder.java
+++ src/main/src/org/compass/core/mapping/osem/builder/SearchableIdComponentMappingBuilder.java
@@ -16,53 +16,110 @@
 
 package org.compass.core.mapping.osem.builder;
 
+import org.compass.core.converter.Converter;
 import org.compass.core.mapping.Cascade;
 import org.compass.core.mapping.osem.IdComponentMapping;
 
 /**
+ * Specifies a searchable id component on property or field of the {@link SearchableMappingBuilder} class.
+ *
+ * <p>A searchable id component is a class field/property that reference another class, which
+ * content need to be embedded into the content of its searchable class and
+ * represents one of its ids.
+ *
  * @author kimchy
  */
 public class SearchableIdComponentMappingBuilder {
 
     final IdComponentMapping mapping;
 
+    /**
+     * Construct a new id component mapping buidler for the specified searchable class property name.
+     */
     public SearchableIdComponentMappingBuilder(String name) {
         mapping = new IdComponentMapping();
         mapping.setName(name);
         mapping.setPropertyName(name);
     }
 
+    /**
+     * The reference alias that points to the searchable class (either defined using
+     * annotations or xml). Not required since most of the times it can be automatically
+     * detected.
+     */
     public SearchableIdComponentMappingBuilder refAlias(String... refAlias) {
         mapping.setRefAliases(refAlias);
         return this;
     }
 
+    /**
+     * The depth of cyclic component references allowed. Defaults to 1.
+     */
     public SearchableIdComponentMappingBuilder maxDepth(int maxDepth) {
         mapping.setMaxDepth(maxDepth);
         return this;
     }
 
+    /**
+     * An optional prefix that will be appended to all the component referenced class mappings.
+     */
     public SearchableIdComponentMappingBuilder prefix(String prefix) {
         mapping.setPrefix(prefix);
         return this;
     }
 
+    /**
+     * Sets the acessor the will be used for the class property. Defaults to property (getter
+     * and optionally setter).
+     */
     public SearchableIdComponentMappingBuilder accessor(Accessor accessor) {
         return accessor(accessor.toString());
     }
 
+    /**
+     * Sets the acessor the will be used for the class property. Defaults to property (getter
+     * and optionally setter). Note, this is the lookup name of a {@link org.compass.core.accessor.PropertyAccessor}
+     * registered with Compass, with two default ones (custom ones can be easily added) named <code>field</code>
+     * and <code>property</code>.
+     */
     public SearchableIdComponentMappingBuilder accessor(String accessor) {
         mapping.setAccessor(accessor);
         return this;
     }
 
+    /**
+     * Sets if this mapping will override another mapping with the same name. Defaults to
+     * <code>true</code>.
+     */
     public SearchableIdComponentMappingBuilder overrideByName(boolean override) {
         mapping.setOverrideByName(override);
         return this;
     }
 
+    /**
+     * Controls which operations will cascade from the parent searchable class to the referenced component
+     * based class. Defaults to no cascading.
+     */
     public SearchableIdComponentMappingBuilder cascade(Cascade... cascade) {
         mapping.setCascades(cascade);
+        return this;
+    }
+
+    /**
+     * Sets the mapping converter lookup name. Defaults to
+     * {@link org.compass.core.converter.mapping.osem.ComponentMappingConverter}.
+     */
+    public SearchableIdComponentMappingBuilder mappingConverter(String mappingConverter) {
+        mapping.setConverterName(mappingConverter);
+        return this;
+    }
+
+    /**
+     * Sets the mapping converter. Defaults to
+     * {@link org.compass.core.converter.mapping.osem.ComponentMappingConverter}.
+     */
+    public SearchableIdComponentMappingBuilder mappingConverter(Converter mappingConverter) {
+        mapping.setConverter(mappingConverter);
         return this;
     }
 }
