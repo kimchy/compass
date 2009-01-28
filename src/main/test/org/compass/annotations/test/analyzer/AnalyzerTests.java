ad66a3df1800830318f9632ef9399298b7f16be0
kimchy <kimchy@gmail.com>
2009-01-28 00:48:31 +0000

    fix test

--- src/main/test/org/compass/annotations/test/analyzer/AnalyzerTests.java
+++ src/main/test/org/compass/annotations/test/analyzer/AnalyzerTests.java
@@ -56,12 +56,12 @@ public class AnalyzerTests extends AbstractAnnotationsTestCase {
         a.analyzer = null;
         try {
             session.save(a);
+            tr.commit();
             fail();
         } catch (SearchEngineException e) {
 
         }
-        tr.commit();
-        session.close();
+        session.rollback();
     }
 
 }
